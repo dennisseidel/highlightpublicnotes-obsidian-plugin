93991f66df41d214091d951a4a1e1dd58e032eea
Dennis Seidel <den.seidel@gmail.com>
2021-04-14 12:31:52 +0200

    ci: create changelog before release

--- devops/release.sh
+++ devops/release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 npm run build
-version=$(cat manifest.json | jq -r ".version")
 npx standard-version -v
+version=$(cat manifest.json | jq -r ".version")
 gh release create ${version} -F CHANGELOG.md manifest.json main.js styles.css
